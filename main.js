6f2a9ee4668a9de295d7ad2986bb2a10e4bed01a
nickdebakey <nick@nickdebakey.com>
2026-04-03 17:36:22 -0400

    lots of adjustmnets made and great progress !

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-function lightMode() {
-  var element = document.body;
-  var lightModeHidden = false;
-  element.classList.toggle("light-mode");
-  function action() {
-    hidden = !hidden;
-    if (hidden) {
-      document.getElementById("").style.visibility = "hidden";
-    } else {
-      document.getElementById("").style.visibility = "visible";
-    }
-  }
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    $('#header img').toggle();
 }
 
 function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
 
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }

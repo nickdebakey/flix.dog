66e0855be7d09ee05c4d51d207f3b74f2e24f089
nickdebakey <nick@nickdebakey.com>
2026-04-03 19:07:58 -0400

    everything should be fixed now.....

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
     $('#header img').toggle();
+    $('#darkModeBtn p').toggle();
     $('.socialLinks').toggleClass("socialLinksLight");
 }
